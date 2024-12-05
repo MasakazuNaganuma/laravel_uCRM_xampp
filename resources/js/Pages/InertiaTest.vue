f289d371682321c3518737ecadbf99c3c14590da
Masakazu Naganuma <masakazu.naganuma@gmail.com>
2024-12-05 21:14:02 +0900

    19. Link名前付きルート

--- resources/js/Pages/InertiaTest.vue
+++ resources/js/Pages/InertiaTest.vue
@@ -1,9 +1,12 @@
 <script setup>
-    import { Link } from '@inertiajs/inertia-vue3';
+    import { Link } from '@inertiajs/vue3';
 </script>
 
 <template>
     Inertiaテストです。<br>
     <a href="/">aタグでWelcomeに移動</a><br>
-    <Link href="/">LinkでWelcomeに移動</Link> 
+    <Link href="/">LinkでWelcomeに移動</Link><br>
+
+    <Link :href="route('inertia.index')">名前付きルートの確認です</Link>
+
 </template>
